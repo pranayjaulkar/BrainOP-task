338d6df0341b873c4d8cb356dec11edb92e93723
pranayjaulkar <pranaypj13@gmail.com>
2024-06-04 16:44:39 +0530

    changes

--- server/utils/schema.js
+++ server/utils/schema.js
@@ -3,10 +3,13 @@ const joi = require("joi");
 const userSchema = joi.object({
   _id: joi.allow(),
   name: joi.string().required(),
-  email: joi.string().email({ minDomainSegments: 2 }).required(),
+  email: joi
+    .string()
+    .email({ tlds: { allow: false } })
+    .required(),
   password: joi.string().required(),
   profilePicture: joi.object({ url: joi.string().required() }),
   createdAt: joi.date().allow(),
 });
 
-module.exports = {  userSchema };
+module.exports = { userSchema };
